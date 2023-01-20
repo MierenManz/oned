42c08e3b8ee47fca625bf7e0da4d3624222fc6f2
MierenManz <christiaanmens@gmail.com>
2023-01-20 20:07:29 +0100

    Revert "fix(napi): Implement `napi_threadsafe_function` ref and unref (#17304)"
    This reverts commit dd2829be0c1f5c40ec38a045ea0a14bec34a82c5.

--- test_napi/src/lib.rs
+++ test_napi/src/lib.rs
@@ -18,7 +18,6 @@ pub mod primitives;
 pub mod promise;
 pub mod properties;
 pub mod strings;
-pub mod tsfn;
 pub mod typedarray;
 
 #[macro_export]
@@ -122,7 +121,6 @@ unsafe extern "C" fn napi_register_module_v1(
   object_wrap::init(env, exports);
   callback::init(env, exports);
   r#async::init(env, exports);
-  tsfn::init(env, exports);
   init_cleanup_hook(env, exports);
 
   exports
