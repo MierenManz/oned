e1091055603d2ca1bc3d5020a626bc9a0dd16c68
MierenManz <christiaanmens@gmail.com>
2023-01-20 20:16:02 +0100

    Revert "fix(npm): improve exports resolution when type checking (#17071)"
    This reverts commit 9e977cd6aaabc6618421578654becafa1ab611f5.

--- cli/tests/testdata/npm/types/main.ts
+++ cli/tests/testdata/npm/types/main.ts
@@ -1,9 +1,6 @@
 import type { Fizzbuzz } from "npm:@denotest/types";
 import type { SomeInterface } from "npm:@denotest/types_imported";
 import type { Foobar as FooInterface } from "npm:@denotest/types_imported/subpath";
-import { getClient } from "npm:@denotest/types-exports-subpaths/client";
-import { entryB } from "npm:@denotest/types-exports-subpaths/entry-b";
-import { entryC } from "npm:@denotest/types-exports-subpaths/entry-c";
 
 const foobar: FooInterface = {
   foo: "foo",
@@ -19,7 +16,3 @@ const fizzbuzz: Fizzbuzz = {
   fizz: 1,
   buzz: 2,
 };
-
-const valueA: "test1" = getClient();
-const valueB: "test2" = entryB();
-const valueC: "test3" = entryC();
